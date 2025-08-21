f69bad8c62530381aba11314351eef1f9045612b
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-08-22 00:36:13 +0330

    type fix

--- front-end/types/podcast.ts
+++ front-end/types/podcast.ts
@@ -5,3 +5,25 @@ export interface PodcastShort {
   listens: number;
   createdAt: string;
 }
+
+
+export interface Podcast {
+  _id: string;
+  title: string;
+  thumbnail: string;
+  content: string; // URL to the audio file
+  shortDesc: string;
+  longDesc: string | { blocks: Block[] }; // Can be a JSON string or parsed object
+  related: string[]; // Array of related podcast IDs
+}
+export interface Block {
+  type: "header" | "paragraph" | "list" | "image";
+  data: {
+    level?: number; // For headers
+    text?: string; // For headers and paragraphs
+    style?: "unordered" | "ordered" | "checklist"; // For lists
+    items?: Array<{ content: string; meta?: { checked: boolean } }>; // For lists
+    file?: { url: string }; // For images
+    caption?: string; // For images
+  };
+}
