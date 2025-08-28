a975e9a13b3325f464c083f281f492ef672c3c20
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-08-28 10:00:48 +0330

    auth

--- back-end/routes/resume.routes.js
+++ back-end/routes/resume.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { upload, uploadResume, getResume } = require("../controllers/resume.controller.js");
+const authorizeAdmin = require('../middleware/authorizeAdmin.js');
+const authenticate = require('../middleware/auth.middleware.js');
 
-router.post("/resume/upload", upload.single("resume"), uploadResume);
+router.post("/resume/upload", authenticate, authorizeAdmin, upload.single("resume"), uploadResume);
 router.get("/resume", getResume);
 
 module.exports = router;
